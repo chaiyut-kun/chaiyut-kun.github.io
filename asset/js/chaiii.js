1103ffa75334e097ae77572d5c6bdefd5b0b467e
chaiyut-kun <135094423+chaiyut-kun@users.noreply.github.com>
2023-08-09 04:09:53 +0700

    Add files via upload

--- asset/js/chaiii.js
+++ asset/js/chaiii.js
@@ -1,16 +1,36 @@
-let answer = "picoCTF{1234}"
+// let answer = "picoCTF{1234}"
 
 
-function checkBool(){
-    let a = prompt("Enter your flag")
-    if (a == answer){
-        alert("Your flag is True")
-    }
-    else{
-        alert("Your flag is False")
-        return checkBool();
-    }
+// function checkBool(){
+//     let a = prompt("Enter your flag")
+//     if (a == answer){
+//         alert("Your flag is True")
+//     }
+//     else{
+//         alert("Your flag is False")
+//         return checkBool();
+//     }
+// }
+
+// checkBool();
+
+
+let aboutME = document.getElementById('aboutME')
+let descibe1 = document.getElementById('describe1')
+let descibe2 = document.getElementById('describe2')
+function toAbout(){
+    aboutME.classList.add('Headabout')
+    descibe1.classList.remove('describe')
+    descibe1.classList.add('describe2')
+    descibe2.classList.remove('describe')
+    descibe2.classList.add('describe2')
+    
+    
 }
 
-checkBool();
-
+// function toggle(){
+//     togglecolo.classList.toggle('addcolor');
+//     check_toggle();
+//     contains();
+        
+// }
